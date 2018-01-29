1ed2c1fe153f628776243549ca66862d41558356
Vo Thanh Tung <thanhtungka91@gmail.com>
2018-01-29 23:54:40 +0900

    initial setting crawler module

--- config/routes.php
+++ config/routes.php
@@ -56,6 +56,8 @@ Router::scope('/', function (RouteBuilder $routes) {
      */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
+    $routes->connect('/crawler', ['controller' => 'Crawlers', 'action' => 'index']);
+
     /**
      * Connect catchall routes for all controllers.
      *
